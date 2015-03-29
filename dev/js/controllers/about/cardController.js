35bf6125e8e51dd2f2a4d253fcd8ffd110e4d75d
David Aghassi <david.aghassi@gmail.com>
2015-03-29 14:58:23 -0400

    Started changing about.jade for ng-repeat support
    Using ng-repeat to point about.jade to json file with data for cards.

--- dev/js/controllers/about/cardController.js
+++ dev/js/controllers/about/cardController.js
@@ -1,7 +1,8 @@
 var about = angular.module('swingclub');
 
 about.controller("cardController", function($scope, $http){
-    $http.get("../../../json/about-cards.json").success(function(links){
-        $scope.data = data;
+    $http.get("../../../json/about-cards.json").success(function(data){
+        $scope.cards = data;
+        $scope.urls = data.urls
     });
 });
