a81aa68d128f4fc7a506e5ae5d5d222471140446
Aghassi <david.aghassi@gmail.com>
2015-04-30 00:18:04 -0400

    Wrote basic code for nanoGallery, untested

--- src/js/scripts/loadSparX2015.js
+++ src/js/scripts/loadSparX2015.js
@@ -1,24 +1,15 @@
-var photoswype = {
-    /**
-     * Makes an ajax call to picasa album to gather photos
-     * Sets photos to the photoswype element in a json form
-     **/
-    init: function() {
-        var pswpElement = document.querySelectorAll('.pswp')[0];
-        var items = {};
+$(document).ready(function () {
+    $("#gallery").nanoGallery({
+        kind:'picasa',
+        userID:'cwruswing',
+        albums:'6135497289919570833',
+        thumbnailWidth: 200,
+        thumbnailHeight: 175,
 
-        $.ajax({
-            type: 'GET',
-            url: 'https://picasaweb.google.com/data/feed/base/user/cwruswing/albumid/6135497289919570833?alt=json&kind=photo',
-            success: function(data) {
-                $.each(data.feed.entry, function(i, photo) {
-                    var item = {};
-                    item.src = photo.media$group.media$content[0].url;
+         paginationMaxLinesPerPage: 3,
 
-                    items.push(item);
-                });
-            },
-            dataType: 'json',
-        });
-    }
-};
+        thumbnailHoverEffect: 'labelSlideUp,borderLighter',
+        thumbnailLabel: {display: true, align: 'center'},
+        theme: 'clean',
+    });
+});
