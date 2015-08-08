7b6e709c337694153dfbda28a898af3db387d85b
David Aghassi <david.aghassi@gmail.com>
2015-08-07 23:41:37 -0400

    Revert "Revert "Edited deploy.sh""
    This reverts commit ec8a92e8c1cc736293b83e91c33343fe0dcc60b1.

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ phrase=$1
 echo $phrase
 
 # Delete all the old files
-find prod -type d | xargs -n1 -I % curl -X DELETE -k https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
+find prod -type d -not -name prod | awk -F'prod/' '{print $2}' | xargs -n1 -I % curl -X DELETE -k https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
 
 # Deploy all the new ones
-find prod -type d -exec basename {} \; | xargs -n1 -I % echo curl -X MKCOL -k https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
+find prod -type d -not -name prod | awk -F'prod/' '{print $2}' | xargs -n1 -I % curl -X MKCOL -k https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
 
-find prod -type f | xargs -n1 -I % curl -X PUT -k -T % https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
+find prod -type f | awk -F'prod/' '{print $2}' | xargs -n1 -I % curl -X PUT -k -T % https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
