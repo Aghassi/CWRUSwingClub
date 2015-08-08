cf947d661c3bb7b8a6fa1bd452be0c159b06f301
David Aghassi <david.aghassi@gmail.com>
2015-08-07 23:24:06 -0400

    Updated .travis.yml and deploy.sh
    -  Deploy.sh no longer echos phrase being passed
       in
    -  .travis.yml updated with deploy task for master
       This needs to be tested

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 phrase=$1
-echo $phrase
 
 # Delete all the old files
 find prod -type d -not -name prod | awk -F'prod/' '{print $2}' | xargs -n1 -I % curl -X DELETE -k https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
