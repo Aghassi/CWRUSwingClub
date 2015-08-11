d44e67d65b8d20a9aaa7b0fec332d824ecbe8c59
David Aghassi <david.aghassi@gmail.com>
2015-08-11 10:56:02 -0400

    Added secure property to travis
    -  Added secure property for deployment
    -  updated deploy script to delete existing
       content and created new folders, then move
       new content to new folders.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-phrase=$1
+user=$1
+phrase=$2
 
 # Delete all the old files
-find prod -type d -not -name prod | awk -F'prod/' '{print $2}' | xargs -n1 -I % curl -X DELETE -k https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
+ls prod | xargs -n1 -I % curl -X DELETE -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/test/%
 
 # Deploy all the new ones
-find prod -type d -not -name prod | awk -F'prod/' '{print $2}' | xargs -n1 -I % curl -X MKCOL -k https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
+find prod -type d | xargs -n1 -I % curl -X MKCOL -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/%
 
-find prod -type f | awk -F'prod/' '{print $2}' | xargs -n1 -I % curl -X PUT -k -T % https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
+find prod -type f | xargs -n1 -I % curl -X PUT -k -T % https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/%
+
+
+# Move all the new data to proper folders
+# 1. Make the folder
+# 2. Move the data from prod
+# 3. Delete prod
+curl -X MKCOL -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/test
+ls prod -I index.html | xargs -n1 -I % curl -X MKCOL -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/test/%
+
+ls prod | xargs -n1 -I % curl -X MOVE -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/prod/% --header "Destination: https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/test/%" --header "Overwrite: T"
+
+curl -X DELETE -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/prod
+curl -X DELETE -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/test/prod/
