78281fb08d70d20486b80886cb42f0ecc9f8c338
David Aghassi <david.aghassi@gmail.com>
2015-08-11 10:23:19 -0400

    Edited deploy.sh
    -  Still working on getting this right
    -  Changed to delete all things first, then to
       upload and move things.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-phrase=$1
-echo $phrase
+user=$1
+phrase=$2
 
 # Delete all the old files
-find prod -type d | xargs -n1 -I % curl -X DELETE -k https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
+ls prod | xargs -n1 -I % curl -X DELETE -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/%
 
 # Deploy all the new ones
-find prod -type d -exec basename {} \; | xargs -n1 -I % echo curl -X MKCOL -k https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
+find prod -type d | xargs -n1 -I % curl -X MKCOL -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/%
 
-find prod -type f | xargs -n1 -I % curl -X PUT -k -T % https://dsa28:$phrase@www.case.edu:8000/dsa28/swingclub/%
+find prod -type f | xargs -n1 -I % curl -X PUT -k -T % https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/%
+
+
+# Move all the new data to proper folders
+# 1. Make the folder
+# 2. Move the data from prod
+# 3. Delete prod
+ls prod | xargs -n1 -I % curl -X MKCOL -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/%
+
+ls prod | xargs -n1 -I % curl -X MOVE -k https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/prod/% --header "Destination: https://$user:$phrase@www.case.edu:8000/dsa28/swingclub/%" --header "Overwrite: T"
